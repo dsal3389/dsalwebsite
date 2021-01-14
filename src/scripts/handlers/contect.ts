953f83a91f0584e02d454eecaf696c66b1b75dfa
dsal3389 <dsal3389@gmail.com>
2021-01-14 22:48:36 +0200

    many
    added some dynamic content, the content page will fetch the data from
    the content dir

--- src/scripts/handlers/contect.ts
+++ src/scripts/handlers/contect.ts
@@ -1,10 +1,46 @@
-import { Handler } from "./handler";
+import { appConfig } from '../app.conf';
+import { Handler, HandlerInitComponent } from './handler';
 
+
+interface socialsResponse{
+    "socials": Array<string[]>;
+}
 
 @Handler({
     targetHTML: 'main',
     templateURL: 'contect.html'
 })
-export class ContectHandler{
-    // empty for now
+export class ContectHandler implements HandlerInitComponent{
+    
+    /**
+     * fetching the data from the "backend" (git folder called content) and placing the
+     * response in the HTML
+     */
+    onReady(){
+        const socials = new XMLHttpRequest();
+        socials.open('GET', `${appConfig.productionDomain}/${appConfig.productionContent}` + '/about/main.json');
+        socials.responseType = 'json';
+        socials.send();
+
+        (() => {
+            return new Promise<socialsResponse>((resolve, reject) => {
+                socials.onreadystatechange = () => {
+                    if(socials.readyState == XMLHttpRequest.DONE)
+                        resolve(socials.response);
+                }
+            });
+        })().then((res) => {
+            const socialHTML = <HTMLUListElement>document.getElementById('social-list');
+            let counter = 0;
+
+            res['socials'].forEach(s => {
+                const social = <HTMLLIElement>document.createElement('li');
+                social.innerHTML = `
+                    <p>[0x${counter.toString(16)}] <a href="${s[1]}" target="blank">${s[0]}</a></p>
+                `;
+                socialHTML.appendChild(social);
+                counter++;
+            });
+        });
+    }
 }

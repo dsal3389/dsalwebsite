41a750258861d6aa9bdc544c4c45c9d6445612b9
dsal3389 <dsal3389@gmail.com>
2021-05-09 08:43:18 +0300

    rebuild after being lazy

--- src/scripts/handlers/resources.ts
+++ src/scripts/handlers/resources.ts
@@ -9,8 +9,33 @@ import { Handler, HandlerInitComponent } from "./handler";
 export class ResourcesHandler implements HandlerInitComponent{
     
     onReady(){
-        fetchJSON('resources.json').then(data => {
-            console.log(data)
+        const resourceList = <HTMLDivElement>document.getElementById('resources-list');
+
+        fetchJSON<any>('resources.json').then(data => {
+            resourceList.innerHTML = '';
+            resourceList.append(this.createList("/", data));
         });
+    }
+
+    createList(name:string, list:any){
+        const div = <HTMLDivElement>document.createElement('div');
+        const ul  = <HTMLUListElement> document.createElement('ul');
+        div.innerHTML = `<p class="folder-name">${name}</p>`;
+        div.appendChild(ul);
+
+        for(let name in list){
+            let element;
+        
+            if(list[name] instanceof Object){
+                element = this.createList(name, list[name]);
+                element.getElementsByTagName('p')[0].prepend(document.createElement('span').innerText="└ ─ ");
+            } else {
+                element =<HTMLLIElement>document.createElement('li');
+                element.innerHTML = `<p><span>├── </span><a href="${list[name]}" target="blank">/${name}</a></p>`;
+            }
+
+            ul.appendChild(element);
+        }
+        return div;
     }
 }
